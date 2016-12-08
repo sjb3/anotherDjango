76365d51b140cadaf5ca921deb3df785ab3b587a
Sung J Byun <sungjbyun@D-108-179-156-93.dhcp4.washington.edu>
2016-12-08 13:31:17 -0800

    fixed the error

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
 from blog.models import Post
 
-urlpatterns = [ url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:15],
+urlpatterns = [ url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:10],
     template_name="blog/blog.html"))]
