a3802ad10f5e2516f019507b47d0903b76c5693c
Sung J Byun <sungjbyun@D-173-250-241-219.dhcp4.washington.edu>
2016-12-07 20:23:26 -0800

    adding blog

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+def contact(request):
+    return render(request, 'personal/basic.html', {'content': ['contact me:', 'klofseattle@hotmail.com','/ 206)327-7850']})
