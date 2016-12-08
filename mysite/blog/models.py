e4c78ffd6bc25926ee5e9c937e583923985dbdde
Sung J Byun <sungjbyun@D-173-250-241-219.dhcp4.washington.edu>
2016-12-07 20:57:35 -0800

    blog.html-done

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
